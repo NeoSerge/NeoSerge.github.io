a607e37131bebb84059f5478f92fe785554856b4
Giovanni Bisogno <neoserge@gmail.com>
2022-02-16 20:01:47 -0600

    prueba gltf controles

--- complete/lecture3_11/app.js
+++ complete/lecture3_11/app.js
@@ -61,7 +61,7 @@ class App{
 		// Load a GLTF resource
 		loader.load(
 			// resource URL
-			`knight2.glb`,
+			`toalla.gltf`,
 			// called when the resource is loaded
 			function ( gltf ) {
 				const object = gltf.scene.children[5];
@@ -86,7 +86,7 @@ class App{
 				self.knight = new Player(options);
                 self.knight.object.visible = false;
 				
-				self.knight.action = 'Dance';
+				//self.knight.action = 'Dance';
 				const scale = 0.003;
 				self.knight.object.scale.set(scale, scale, scale); 
 				
